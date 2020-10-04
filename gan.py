b350edd7cf3ed469edba768c24ef301e17d837bf
Vedant Varshney <vedantvarshney1408@gmail.com>
2020-10-04 17:48:14 +0100

    Added functionality for TTUR to GAN. Seaborn removed.

--- gan.py
+++ gan.py
@@ -48,7 +48,7 @@ class GAN:
         - min_filters - number of starting convolutional filters (optional)
         - latent_dims - number of latent dimensions for Generator (optional)
         - strides - strides for convolutions (optional)
-        - lr - learning rate (optional)
+        - lr - learning rate (optional). (gen_lr, disc_lr) or int.
         """
         assert len(x_shape) == 3
 
@@ -58,7 +58,14 @@ class GAN:
         self.min_filters = min_filters
         self.latent_dims = latent_dims
         self.strides = strides
-        self.lr = lr
+
+        if isinstance(lr, float):
+            self.gen_lr = lr
+            self.disc_lr = lr
+        else:
+            assert hasattr(lr, "__len__")
+            assert len(lr) == 2
+            self.gen_lr, self.disc_lr = lr
 
         self.history = None
 
@@ -66,20 +73,11 @@ class GAN:
         self.generator = self.create_generator()
         self.combined = self.create_combined(verbose=verbose)
 
-    def reset_models(self, model="both"):
-        if model == "generator":
-            self.generator = self.create_generator()
-            raise ModelResetWarning("Generator did reset")
-        elif model == "discriminator":
-            self.discriminator = self.create_discriminator()
-            raise ModelResetWarning("Discriminator did reset")
-        else:
-            self.generator = self.create_generator()
-            self.discriminator = self.create_discriminator()
-            raise ModelResetWarning("Generator and Discriminator did reset")
-
+    def reset_models(self):
+        self.generator = self.create_generator()
+        self.discriminator = self.create_discriminator()
         self.combined = self.create_combined()
-        raise ModelResetWarning("Combined model did reset")
+        raise ModelResetWarning("All models did reset.")
 
     @staticmethod
     def add_activation(x, activation):
@@ -119,7 +117,7 @@ class GAN:
 
         if compile:
             discriminator.compile(loss="binary_crossentropy",
-                optimizer=Adam(lr=self.lr), metrics=["mae"])
+                optimizer=Adam(lr=self.disc_lr), metrics=["mae"])
 
         return discriminator
 
@@ -186,7 +184,7 @@ class GAN:
         self.discriminator.trainable = False
 
         combined.compile(loss='binary_crossentropy',
-            optimizer=Adam(lr=self.lr), metrics=['mae'])
+            optimizer=Adam(lr=self.gen_lr), metrics=['mae'])
 
         if verbose:
             print(self.generator.summary(), "\n")
@@ -196,7 +194,8 @@ class GAN:
         return combined
 
     def train(self, real_train, num_epochs, batch_size,
-            disc_updates=1, gen_updates=1, show_imgs=True, save_imgs=True):
+            disc_updates=1, gen_updates=1, show_imgs=True, save_imgs=True,
+            labels=(0, 1)):
         """
         Arguments:
         - real_train - preprocessed training examples of real data.
@@ -209,6 +208,7 @@ class GAN:
         Generator before switching to the Discriminator (int)
         - show_imgs - generate and display progress images (once per epoch) (bool)
         - save_imgs - save images in run directory (bool)
+        - labels - (negative label, positive label)
 
         Outputs:
         - Updates weights of GAN instance
@@ -246,8 +246,8 @@ class GAN:
                     batch_data = np.concatenate((real_train[random_real_indxs],
                                                 self.generator.predict(random_seed)))
 
-                    discrim_labels = np.concatenate((np.ones([half_batch1, 1]),
-                                                    np.zeros([half_batch2, 1])))
+                    discrim_labels = np.concatenate((np.zeros([half_batch1, 1])+labels[1],
+                                                    np.zeros([half_batch2, 1])+labels[0]))
 
                     shuffle_indxs = np.random.permutation(batch_size)
                     discrim_labels = discrim_labels[shuffle_indxs]
@@ -260,7 +260,8 @@ class GAN:
                 for _ in range(gen_updates):
                     # Create new images (separate from those used to train discriminator)
                     random_seed = np.random.randn(batch_size, self.latent_dims)
-                    gen_loss += self.combined.train_on_batch(random_seed, np.ones([batch_size, 1]))[1]
+                    gen_loss += self.combined.train_on_batch(random_seed,
+                                    np.zeros([batch_size, 1])+labels[1])[1]
 
                 disc_loss /= disc_updates
                 gen_loss /= gen_updates
@@ -272,7 +273,7 @@ class GAN:
             if (show_imgs or save_imgs):
                 random_seed = np.random.randn(1, self.latent_dims)
                 fake_img = self.generator.predict(random_seed).reshape(spatial_dim, spatial_dim)
-                plt.imshow(fake_img)
+                plt.imshow(fake_img, cmap="gray")
                 if save_imgs:
                     plt.savefig(f"{run_dir}/img_epoch{epoch+1}.png")
                 if not show_imgs:
--- history.py
+++ history.py
@@ -1,7 +1,4 @@
 from matplotlib import pyplot as plt
-import seaborn as sns
-
-sns.set()
 
 class History:
     def __init__(self, run_id, disc_loss=[], gen_loss=[]):
@@ -13,6 +10,6 @@ class History:
         plt.title(f"Run {str(self.run_id)}")
         plt.xlabel("Steps")
         plt.ylabel("Loss")
-        plt.plot(self.disc_loss, label="Discriminator")
-        plt.plot(self.gen_loss, label="Generator")
+        plt.plot(self.disc_loss, label="Discriminator", alpha=0.8)
+        plt.plot(self.gen_loss, label="Generator", alpha=0.8)
         plt.show()
